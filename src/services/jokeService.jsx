2259f620978d4e7efe096e02775099f15969fd67
Joshua Smith <mtgmirage@protonmail.com>
2024-10-21 11:13:46 -0500

    added tellOrUntellJoke function to jokeServices.jsx

--- src/services/jokeService.jsx
+++ src/services/jokeService.jsx
@@ -14,3 +14,13 @@ export const postNewJoke = (theJoke) => {
 export const getAllJokes = () => {
     return fetch("http://localhost:8088/jokes").then(res => res.json())
 }
+
+export const tellOrUntellJoke = (editedJoke) => {
+    fetch("http://localhost:8088/jokes", {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(editedJoke)
+    })
+}
